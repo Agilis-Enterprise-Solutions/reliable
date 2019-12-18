e51776c357bb3aa40358bb6525e29723e3adce86
Agilis <36151720+Agilis-Enterprise-Solutions@users.noreply.github.com>
2019-12-18 17:08:17 +0800

    Update default names

--- report_rl/models/vendor_bill.py
+++ report_rl/models/vendor_bill.py
@@ -5,6 +5,6 @@ _logger = logging.getLogger(__name__)
 class APV(models.Model):
     _inherit='account.invoice'
 
-    requested_by = fields.Char(string="Prepared By", default="Jane Frances Tubello")
-    approved_by = fields.Char(string="Approved By", default="Portia Cardel")
-    checked_by = fields.Char(string="Checked By", default="Jayson Tadeo")
+    requested_by = fields.Char(string="Prepared By", default="Ms. Ada")
+    approved_by = fields.Char(string="Approved By", default="Mae Ramos")
+    checked_by = fields.Char(string="Checked By", default="BAVjr/BTN")
