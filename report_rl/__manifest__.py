17a467c9ff4f7188be1a9417d6b6b19c81669bdf
ralf-agilis <ralf@biz1global.com>
2019-12-26 16:33:18 +0800

    Update APV account report

--- report_rl/__manifest__.py
+++ report_rl/__manifest__.py
@@ -9,7 +9,7 @@
     'sequence': 1,
     'version': '1.0',
     'depends': [
-        'base','account_accountant'
+        'base','account','account_accountant'
         ],
     'data': [
         'report/apv.xml',
