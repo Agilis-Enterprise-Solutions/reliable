0ac9941b41a4d38db1ac5e0aea9349f48489033c
ralf-agilis <ralf@biz1global.com>
2020-02-28 19:53:49 +0800

    Add OR No. in group Payment

--- reliable_invoice/models/account_invoice.py
+++ reliable_invoice/models/account_invoice.py
@@ -2,6 +2,7 @@
 
 from odoo import api, fields, models, _
 import logging
+from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
@@ -15,3 +16,21 @@ class AccountPaymentOfficialReceipt(models.Model):
     _inherit="account.payment"
 
     official_receipt_no = fields.Char(string="Official Receipt No.")
+
+class OfficialReceiptGroup(models.TransientModel):
+    _inherit = "account.register.payments"
+
+    official_receipt_no = fields.Char(string="Official Receipt No.")
+
+    @api.multi
+    def create_payments(self):
+        res = super(OfficialReceiptGroup, self).create_payments()
+        payment = self.env['account.payment'].search([])
+        for k,v in res.items():
+            if k == "res_id":
+                for rec in payment:
+                    if rec.id == v:
+                        rec.write({
+                            'official_receipt_no': self.official_receipt_no
+                        })
+        return res
