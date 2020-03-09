75051c9c9e254dace85e469d6bca878467bafc19
ralf-agilis <ralf@biz1global.com>
2020-03-09 17:28:42 +0800

    Make invoice be multiple selections

--- withholding_tax_report/models/withholding_tax.py
+++ withholding_tax_report/models/withholding_tax.py
@@ -6,7 +6,7 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class AccountPrintWithholdingTax(models.TransientModel):
+class AccountPrintWithholdingTax(models.Model):
     _name = "account.withholding.tax"
     _description = "Print Withholding Tax"
 
@@ -14,34 +14,36 @@ class AccountPrintWithholdingTax(models.TransientModel):
     name = fields.Char("Ref", readonly=True, store=True)
     date_from = fields.Date("From", required=True)
     date_to = fields.Date("To", required=True)
-    partner_id = fields.Many2one('res.partner',"Payee's Name", required=True)
+    partner_id = fields.Many2one('res.partner',
+                                  string="Payees Name", required=True)
     representative_id = fields.Many2one('hr.employee',
                                         "Authorized Representative",
                                         required=True)
-    invoice_id = fields.Many2one('account.invoice', "Invoice", required=True)
+    invoice_id = fields.Many2many('account.invoice',
+                                  string="Invoice", required=True)
 
     state = fields.Selection([
         ('draft', 'Draft'),
         ('generate', 'Generated'),
     ], string="Status", default="draft", readonly=True, copy=False)
 
-    withholding_ids = fields.One2many('account.withholding.tax.lines','withholding_id',
+    withholding_ids = fields.One2many('account.withholding.tax.lines','withholding_id_line',
                                       store=True)
 
-    @api.onchange('invoice_id')
-    def _duplicate_invoice(self):
-        if self.invoice_id:
-            duplicate = self.search([('invoice_id', '=', self.invoice_id.id),
-                                     ('id', '!=', self._origin.id),
-                                     ('state', '=', 'generate')])
-            if duplicate:
-                return {
-                    'warning': {
-                        'title': "Duplicate Entry",
-                        'message': """This Invoice had been already Generated.
-                        Please Check the other records for Reference"""
-                    }
-                }
+    # @api.onchange('invoice_id')
+    # def _duplicate_invoice(self):
+    #     if self.invoice_id:
+    #         duplicate = self.search([('invoice_id', '=', self.invoice_id.id),
+    #                                  ('id', '!=', self._origin.id),
+    #                                  ('state', '=', 'generate')])
+    #         if duplicate:
+    #             return {
+    #                 'warning': {
+    #                     'title': "Duplicate Entry",
+    #                     'message': """This Invoice had been already Generated.
+    #                     Please Check the other records for Reference"""
+    #                 }
+    #             }
 
     @api.model
     def create(self, vals):
@@ -54,51 +56,37 @@ class AccountPrintWithholdingTax(models.TransientModel):
         for i in self.withholding_ids:
             i.unlink()
 
-        duplicate = self.search([('invoice_id', '=', self.invoice_id.id),
-                                 ('id', '!=', self.id),
-                                 ('state', '=', 'generate')])
-        if duplicate:
-            raise UserError('''This Invoice had been already Generated.
-            Please Check the other records for Reference''')
-
         data = []
         branch_code = ""
         for i in self:
-            if i.partner_id.vat:
-                branch_code = str(i.partner_id.vat[-3:])
-            address = '%s %s %s'%(i.partner_id.street if i.partner_id.street else "",
-                                  i.partner_id.street2 if i.partner_id.street2 else "",
-                                  i.partner_id.city if i.partner_id.city else "")
-            # tax_id = [tax.name for tax in i.invoice_id.tax_line_ids][0]
-            # tax = self.env['account.tax'].search([('name', '=', str(tax_id))])
-            # val= {
-            #     'date' : date.today(),
-            #     'vendor_tin' : i.partner_id.vat,
-            #     'branch_code' : branch_code,
-            #     'company_name' : i.partner_id.name,
-            #     'address' : address,
-            #     'tax_id' : tax.id,
-            #     'base_amount' : [tax.base for tax in i.invoice_id.tax_line_ids][0],
-            #     'ewt_rate' : [tax.percentage for tax in i.invoice_id.tax_line_ids][0],
-            #     'tax_amount': [tax.amount_total for tax in i.invoice_id.tax_line_ids][0]
-            #     }
-            # data.append([0, 0, val])
+            for invoice in i.invoice_id:
+                duplicate = self.search([('invoice_id', '=', invoice.id),
+                                         ('id', '!=', self.id),
+                                         ('state', '=', 'generate')])
+                if duplicate:
+                    raise UserError('''This Invoice had been already Generated.
+                    Please Check the other records for Reference''')
 
-            tax_id = [tax.name if tax.nature_of_income else tax.nature_of_income for tax in i.invoice_id.tax_line_ids]
-            tax_ids = self.env['account.tax'].search([])
-            for tax in tax_id:
-                for x in tax_ids:
-                    if tax == x.name:
+                for x in invoice.tax_line_ids:
+                    if i.partner_id.vat:
+                        branch_code = str(i.partner_id.vat[-3:])
+                    address = '%s %s %s'%(i.partner_id.street if i.partner_id.street else "",
+                                          i.partner_id.street2 if i.partner_id.street2 else "",
+                                          i.partner_id.city if i.partner_id.city else "")
+
+                    tax_id = self.env['account.tax'].search([('name','=',x.name),
+                                                             ('nature_of_income','!=',False)])
+                    if tax_id:
                         val= {
                             'date' : date.today(),
-                            'vendor_tin' : i.partner_id.vat,
+                            'vendor_tin' : invoice.partner_id.vat,
                             'branch_code' : branch_code,
                             'company_name' : i.partner_id.name,
                             'address' : address,
-                            'tax_id' : x.id,
-                            'base_amount' : [tax.base for tax in i.invoice_id.tax_line_ids][0],
-                            'ewt_rate' : [abs(tax.percentage) for tax in i.invoice_id.tax_line_ids][0],
-                            'tax_amount': [tax.amount_total for tax in i.invoice_id.tax_line_ids][0]
+                            'tax_id' : tax_id.id,
+                            'base_amount' : x.base,
+                            'ewt_rate' : abs(x.amount),
+                            'tax_amount': x.amount_total
                             }
                         data.append([0, 0, val])
 
@@ -106,11 +94,11 @@ class AccountPrintWithholdingTax(models.TransientModel):
                            'state': 'generate'})
 
 
-class AccountPrintWithholdingTaxLines(models.TransientModel):
+class AccountPrintWithholdingTaxLines(models.Model):
     _name = "account.withholding.tax.lines"
     _description = "Withholding Tax Lines"
 
-    withholding_id = fields.Many2one('account.withholding.tax')
+    withholding_id_line = fields.Many2one('account.withholding.tax')
     date = fields.Date('Reporting Month')
     vendor_tin = fields.Char('Vendor TIN')
     branch_code = fields.Char('Branch Code')
